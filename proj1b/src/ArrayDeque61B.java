d256f7389ad3876ddc290ed4fdd6b8487402c598
TOALLY <2649752859@qq.com>
2025-03-08 23:12:26 +0800

    project1b have finished

--- proj1b/src/ArrayDeque61B.java
+++ proj1b/src/ArrayDeque61B.java
@@ -1,74 +1,140 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.lang.Math;
 
-    public class ArrayDeque61B<T> implements Deque61B<T> {
-        T[] base;
-        int orisize = 8;
-        int nextfirst = 0;
-        int nextlast = 0;
-        int size = 0;
+public class ArrayDeque61B<T> implements Deque61B<T> {
+    T[] base;
+    int currentsize = 8;
+    int nextfirst = 0;
+    int nextlast = 0;
+    int size = 0;
 
-        public ArrayDeque61B(){
-            base = (T[]) new Object[orisize];
-        }
-
-
-        public void addFirst(T x) {
-            size += 1;
-            this.base[nextfirst] = x;
-            if (nextfirst == 0){
-                nextlast += 1;
-                nextlast = Math.floorMod(nextlast,orisize);
-            }
-            nextfirst -= 1;
-            nextfirst = Math.floorMod(nextfirst,orisize);
-        }
-
-        @Override
-        public void addLast(T x) {
-            size += 1;
-            this.base[nextlast] = x;
-            if (nextlast == 0){
-                nextfirst -= 1;
-                nextfirst = Math.floorMod(nextfirst,orisize);
-            }
-            nextlast += 1;
-            nextlast = Math.floorMod(nextlast,orisize);
-        }
-
-        @Override
-    public List<T> toList() {
-        return List.of();
+    public ArrayDeque61B(){
+        base = (T[]) new Object[currentsize];
     }
 
-        @Override
-        public boolean isEmpty() {
+
+    public void addFirst(T x) {
+        this.size += 1;
+        this.resize_helperbigger();
+        this.base[nextfirst] = x;
+        if (nextfirst == 0){
+            nextlast += 1;
+            nextlast = Math.floorMod(nextlast,currentsize);
+        }
+        nextfirst -= 1;
+        nextfirst = Math.floorMod(nextfirst,currentsize);
+    }
+
+    @Override
+    public void addLast(T x) {
+        size += 1;
+        this.resize_helperbigger();
+        this.base[nextlast] = x;
+        if (nextlast == 0){
+            nextfirst -= 1;
+            nextfirst = Math.floorMod(nextfirst,currentsize);
+        }
+        nextlast += 1;
+        nextlast = Math.floorMod(nextlast,currentsize);
+    }
+
+    public void resize_helperbigger(){
+        if (this.size > currentsize) {
+            T[] thecopy = (T[]) new Object[currentsize * 2];
+
+            for (int i = 0;i < currentsize ;i ++){
+                int flag = nextfirst + 1 + i;
+                flag = Math.floorMod(flag,currentsize);
+                thecopy[i] = this.base[flag];
+            }
+            this.currentsize = currentsize * 2;
+            this.nextfirst = Math.floorMod( - 1,currentsize);
+            this.nextlast = this.size() - 1;
+            this.base = thecopy;
+        }
+    }
+    public void resize_helpersmaller(){
+        if (this.size < currentsize / 4) {
+            T[] thecopy = (T[]) new Object[currentsize / 2];
+
+            for (int i = 0;i <= this.size ;i ++){
+                int flag = nextfirst + 1 + i;
+                flag = Math.floorMod(flag,currentsize);
+                thecopy[i] = this.base[flag];
+            }
+            this.currentsize = currentsize / 2;
+            this.nextfirst = Math.floorMod( - 1,currentsize);
+            this.nextlast = this.size() + 1 ;
+            this.base = thecopy;
+        }
+    }
+    @Override
+public List<T> toList() {
+    List<T> returnList = new ArrayList<>();
+    for (int i = 0; i < currentsize; i++) {
+        int flag = nextfirst + 1 + i;
+        flag = Math.floorMod(flag,currentsize);
+        returnList.add(this.get(flag));
+    }
+    return returnList;
+}
+
+    @Override
+    public boolean isEmpty() {
+        if (size > 0)
             return false;
-        }
+        return true;
+    }
 
-        @Override
-        public int size() {
-            return 0;
-        }
+    @Override
+    public int size() {
+        return this.size;
+    }
 
-        @Override
-        public T removeFirst() {
+    @Override
+    public T removeFirst() {
+        if (this.size == 0){
             return null;
         }
+        size -= 1;
+        resize_helpersmaller();
+        nextfirst += 1;
+        nextfirst = Math.floorMod(nextfirst,currentsize);
+        T removed_value = this.base[nextfirst];
+        this.base[nextfirst] = null;
 
-        @Override
-        public T removeLast() {
+        return removed_value;
+    }
+
+    @Override
+    public T removeLast() {
+        if (this.size == 0){
             return null;
         }
+        size -= 1;
+        resize_helpersmaller();
+        nextlast -= 1;
+        nextlast = Math.floorMod(nextlast,currentsize);
+        T removed_value = this.base[nextlast];
+        this.base[nextlast] = null;
 
-        @Override
-        public T get(int index) {
+        return removed_value;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= 0 &&index < currentsize)
+        return this.base[index];
+        else {
             return null;
         }
+    }
 
-        @Override
-        public T getRecursive(int index) {
-            return null;
-        }
-        }
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
+    }
+
+}
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -10,6 +10,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
+    int i = 8;
 
 //     @Test
 //     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
@@ -23,11 +24,52 @@ public class ArrayDeque61BTest {
 
     @Test
     //test add first and last
-    public void testadd(){
+    public void testaddand_isEmpty_size(){
         Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.isEmpty()).isTrue();
         lld1.addFirst(1);
         lld1.addLast(2);
         lld1.addFirst(7);
-        lld1.addLast(3);
+        lld1.addLast(3);//1,2,3,null,null,null,null,7
+        assertThat(lld1.isEmpty()).isFalse();
+        assertThat(lld1.size()).isEqualTo(4);
+        assertThat(lld1.toList()).containsExactly(7,1,2,3,null,null,null,null).inOrder();
+    }
+
+    @Test
+    //remove
+    public  void testremove(){
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.removeLast()).isEqualTo(null);
+        assertThat(lld1.removeFirst()).isEqualTo(null);
+        lld1.addFirst(1);
+        lld1.addLast(2);
+        lld1.addFirst(7);
+        lld1.addLast(3);//1,2,3,null,null,null,null,7
+        lld1.removeFirst();
+        lld1.removeLast();
+        assertThat(lld1.toList()).containsExactly(null,1,2,null,null,null,null,null);
+    }
+
+    @Test
+    //test resize
+    public void test_resize(){
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        for (int i = 0; i <= 8; i++){
+            lld1.addLast(i);
+        }
+        assertThat(lld1.toList()).containsExactly(0,1,2,3,4,5,6,7,8,null,null,null,null,null,null,null).inOrder();
+    }
+
+    @Test
+    public void test_resizesmall(){
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        for (int i = 0; i <= 8; i++){
+            lld1.addLast(i);
+        }
+        for (int i = 8;i >= 3; i--){
+            lld1.removeLast();
+        }
+        assertThat(lld1.toList()).containsExactly(0,1,2,null,null,null,null,null).inOrder();
     }
 }
