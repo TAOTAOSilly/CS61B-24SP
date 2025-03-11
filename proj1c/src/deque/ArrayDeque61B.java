4c758f495bac47086740fd1a7b609678eef2b22e
TOALLY <2649752859@qq.com>
2025-03-11 16:13:25 +0800

    list iterator have finished

--- proj1c/src/deque/ArrayDeque61B.java
+++ proj1c/src/deque/ArrayDeque61B.java
@@ -1,5 +1,146 @@
 package deque;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.lang.Math;
 
 public class ArrayDeque61B<T> implements Deque61B<T> {
+    T[] base;
+    int currentsize = 8;
+    int nextfirst = 0;
+    int nextlast = 0;
+    int size = 0;
 
+    public ArrayDeque61B(){
+        base = (T[]) new Object[currentsize];
+    }
+
+
+    public void addFirst(T x) {
+        this.size += 1;
+        this.resize_helperbigger();
+        this.base[nextfirst] = x;
+        if (nextfirst == 0){
+            nextlast += 1;
+            nextlast = Math.floorMod(nextlast,currentsize);
+        }
+        nextfirst -= 1;
+        nextfirst = Math.floorMod(nextfirst,currentsize);
+    }
+
+    @Override
+    public void addLast(T x) {
+        size += 1;
+        this.resize_helperbigger();
+        this.base[nextlast] = x;
+        if (nextlast == 0){
+            nextfirst -= 1;
+            nextfirst = Math.floorMod(nextfirst,currentsize);
+        }
+        nextlast += 1;
+        nextlast = Math.floorMod(nextlast,currentsize);
+    }
+
+    public void resize_helperbigger(){
+        if (this.size > currentsize) {
+            T[] thecopy = (T[]) new Object[currentsize * 2];
+
+            for (int i = 0;i < currentsize ;i ++){
+                int flag = nextfirst + 1 + i;
+                flag = Math.floorMod(flag,currentsize);
+                thecopy[i] = this.base[flag];
+            }
+            this.currentsize = currentsize * 2;
+            this.nextfirst = Math.floorMod( - 1,currentsize);
+            this.nextlast = this.size() - 1;
+            this.base = thecopy;
+        }
+    }
+    public void resize_helpersmaller(){
+        if (this.size < currentsize / 4) {
+            T[] thecopy = (T[]) new Object[currentsize / 2];
+
+            for (int i = 0;i <= this.size ;i ++){
+                int flag = nextfirst + 1 + i;
+                flag = Math.floorMod(flag,currentsize);
+                thecopy[i] = this.base[flag];
+            }
+            this.currentsize = currentsize / 2;
+            this.nextfirst = Math.floorMod( - 1,currentsize);
+            this.nextlast = this.size() + 1 ;
+            this.base = thecopy;
+        }
+    }
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        for (int i = 0; i < currentsize; i++) {
+            int flag = nextfirst + 1 + i;
+            flag = Math.floorMod(flag,currentsize);
+            returnList.add(this.get(flag));
+        }
+        return returnList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size > 0)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (this.size == 0){
+            return null;
+        }
+        size -= 1;
+        resize_helpersmaller();
+        nextfirst += 1;
+        nextfirst = Math.floorMod(nextfirst,currentsize);
+        T removed_value = this.base[nextfirst];
+        this.base[nextfirst] = null;
+
+        return removed_value;
+    }
+
+    @Override
+    public T removeLast() {
+        if (this.size == 0){
+            return null;
+        }
+        size -= 1;
+        resize_helpersmaller();
+        nextlast -= 1;
+        nextlast = Math.floorMod(nextlast,currentsize);
+        T removed_value = this.base[nextlast];
+        this.base[nextlast] = null;
+
+        return removed_value;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= 0 &&index < currentsize)
+            return this.base[index];
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
 }
--- proj1c/src/deque/LinkedListDeque61B.java
+++ proj1c/src/deque/LinkedListDeque61B.java
@@ -1,5 +1,152 @@
 package deque;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
 
-public class LinkedListDeque61B<T> implements Deque61B<T> {
+public class LinkedListDeque61B<T> implements Deque61B<T>,Iterable<T> {
+    public class Node {
+        public Node prev;
+        public Node next;
+        public T item;
 
+        public Node(Node tprev, T t, Node tnext) {
+            prev = tprev;
+            next = tnext;
+            item = t;
+        }
+    }
+
+    public class LinkedlistDeque61BIterator implements Iterator<T> {
+        int WizPos;
+        Node WizPointer;
+
+        public LinkedlistDeque61BIterator() {
+            WizPos = 1;
+            WizPointer = sentinel.next;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return WizPos <= size;
+        }
+
+        @Override
+        public T next() {
+            T returnitem = WizPointer.item;
+            WizPointer = WizPointer.next;
+            WizPos += 1;
+            return returnitem;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new LinkedlistDeque61BIterator();
+    }
+
+
+    int size = 0;
+    private Node sentinel;
+
+    public LinkedListDeque61B() {
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+    }
+
+    // sentinel.prev == last Node;
+    @Override
+    public void addFirst(T x) {
+        size += 1;
+        Node l = sentinel.next;
+
+        sentinel.next = new Node(sentinel, x, l);
+        l.prev = sentinel.next;
+    }
+
+    @Override
+    public void addLast(T x) {
+        size += 1;
+        Node l = sentinel.prev;
+        l.next = new Node(l, x, sentinel);
+        sentinel.prev = l.next;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnlist = new ArrayList<>();
+        Node p = this.sentinel;
+        while (p.next != sentinel) {
+            p = p.next;
+            returnlist.add(p.item);
+        }
+        return returnlist;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        Node p = this.sentinel;
+        if (p.next == sentinel) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public T removeFirst() {
+        Node p = this.sentinel.next;
+        T removedone = p.item;
+        p = p.next;
+        sentinel.next = p;
+        p.prev = sentinel;
+        return removedone;
+    }
+
+    @Override
+    public T removeLast() {
+        Node p = this.sentinel.prev;
+        T removedone = p.item;
+        p = p.prev;
+        p.next = sentinel;
+        sentinel.prev = p;
+        return removedone;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index > this.size) {
+            System.out.print("the list have not the value to the index");
+            return null;
+        }
+        Node p = this.sentinel;
+        int pindex = 0;
+        while (true) {
+            if (pindex == index) {
+                return p.item;
+            }
+            p = p.next;
+            pindex += 1;
+        }
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        Node p = this.sentinel;
+        return getRecusivehelper(p, index);
+    }
+
+    public T getRecusivehelper(Node p, int index) {
+        if (index == 0) {
+            return p.item;
+        }
+        return getRecusivehelper(p.next, index - 1);
+    }
 }
+
+
