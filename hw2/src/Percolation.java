7915eb4d284367c03adc039dde588a8ffb6c3473
TOALLY <2649752859@qq.com>
2025-05-16 13:05:54 +0800

    pass h2 basictest

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -3,32 +3,69 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     // TODO: Add any necessary instance variables.
+    private WeightedQuickUnionUF UF;
+    private int[] Percolations;
+    private int n;
+    private int numberOfOpenSites = 0;
 
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+        UF = new WeightedQuickUnionUF(N * N);
+        Percolations = new int[N * N];
+        n = N;
+        for (int i = 0; i < N * N;i ++) {
+            Percolations[i] = 1;
+        }
     }
 
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+        int site = row * n + col;
+        Percolations[site] = 0;
+        numberOfOpenSites += 1;
+        unionHelper(site);
+    }
+    private void unionHelper(int site) {
+       int[] nearby = {site - n, site + n, site - 1, site + 1};
+       for (int i : nearby){
+           if ( i >= 0 && i < n * n) {
+               if (isOpen(i / n , i % n)) {
+                   UF.union(site,i);
+               }
+           }
+       }
     }
 
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
+        int site = row * n + col;
+        if ( Percolations[site] == 0) {
+            return true;
+        }
         return false;
     }
 
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
+        int site = row * n + col;
+        if (isOpen(row, col)) {
+            for (int i = 0; i < n; i++) {
+                if (UF.connected(i, site)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
-        return 0;
+        return numberOfOpenSites;
     }
 
     public boolean percolates() {
-        // TODO: Fill in this method.
+        int bottomIndex = n - 1;
+        for (int i = 0; i < n; i++) {
+            if (isFull(n - 1,bottomIndex)){
+                return true;
+            }
+            bottomIndex--;
+        }
         return false;
     }
 
