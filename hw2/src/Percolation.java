09a6f86c3c176d6de0887867b667e447a76e3b95
TOALLY <2649752859@qq.com>
2025-05-17 14:59:56 +0800

    finish h2 with correct Percolation threshold.

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -4,27 +4,40 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     // TODO: Add any necessary instance variables.
     private WeightedQuickUnionUF UF;
-    private int[] Percolations;
+    private boolean[] grid;
     private int n;
     private int numberOfOpenSites = 0;
+    private int virtualTop;
+    private int virtualBottom;
 
     public Percolation(int N) {
-        UF = new WeightedQuickUnionUF(N * N);
-        Percolations = new int[N * N];
-        n = N;
-        for (int i = 0; i < N * N;i ++) {
-            Percolations[i] = 1;
+        UF = new WeightedQuickUnionUF(N * N + 2); // 增加虚拟节点
+        virtualTop = N * N;
+        virtualBottom = N * N + 1;
+        // 初始化虚拟节点连接
+        for (int i = 0; i < N; i++) {
+            UF.union(virtualTop, i);       // 虚拟顶节点连接首行
+            UF.union(virtualBottom, N*(N-1) + i); // 虚拟底节点连接末行
         }
+        grid = new boolean[N * N];
+        n = N;
+    }
+
+    private int xyToSite(int row, int col){
+        return row * n + col;
     }
 
     public void open(int row, int col) {
-        int site = row * n + col;
-        Percolations[site] = 0;
-        numberOfOpenSites += 1;
-        unionHelper(site);
+        if (grid[xyToSite(row,col)]) {
+            return; // 已打开的格子直接返回
+        }
+        grid[xyToSite(row,col)] = true;
+        numberOfOpenSites ++;
+        unionHelper(row,col);
     }
-    private void unionHelper(int site) {
-       int[] nearby = {site - n, site + n, site - 1, site + 1};
+    private void unionHelper(int row ,int col) {
+        int site = xyToSite(row,col);
+        int[] nearby = {site - n, site + n, site - 1, site + 1};
        for (int i : nearby){
            if ( i >= 0 && i < n * n) {
                if (isOpen(i / n , i % n)) {
@@ -35,21 +48,16 @@ public class Percolation {
     }
 
     public boolean isOpen(int row, int col) {
-        int site = row * n + col;
-        if ( Percolations[site] == 0) {
+        if ( grid[xyToSite(row,col)]) {
             return true;
         }
         return false;
     }
 
     public boolean isFull(int row, int col) {
-        int site = row * n + col;
-        if (isOpen(row, col)) {
-            for (int i = 0; i < n; i++) {
-                if (UF.connected(i, site)) {
-                    return true;
-                }
-            }
+        int site = xyToSite(row,col);
+        if (isOpen(row,col)) {
+            return UF.connected(site, virtualTop);
         }
         return false;
     }
@@ -59,17 +67,7 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        int bottomIndex = n - 1;
-        for (int i = 0; i < n; i++) {
-            if (isFull(n - 1,bottomIndex)){
-                return true;
-            }
-            bottomIndex--;
-        }
-        return false;
+        return UF.connected(virtualBottom,virtualTop);
     }
-
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
 
 }
